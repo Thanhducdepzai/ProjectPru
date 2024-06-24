9e48f964a78d1d39e1b590d2e46849b2e053e14d
Thanhducdepzai <136815568+Thanhducdepzai@users.noreply.github.com>
2024-06-24 14:20:48 +0700

    Create commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public float speed = 5.0f;
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TakeDamage(20);
+        }
+
+        // Lấy thông tin từ các phím mũi tên
+        float moveHorizontal = 0f;
+        float moveVertical = 0f;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            moveVertical = 1f; // Đi lên
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            moveVertical = -1f; // Đi xuống
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveHorizontal = -1f; // Sang trái
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveHorizontal = 1f; // Sang phải
+        }
+
+        // Tạo vector di chuyển dựa trên thông tin từ các phím mũi tên
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+
+        // Đảm bảo di chuyển theo hướng phù hợp
+        if (movement != Vector3.zero)
+        {
+            movement.Normalize(); // Chuẩn hóa vector di chuyển để không di chuyển nhanh hơn khi đi chéo
+        }
+
+        // Di chuyển nhân vật
+        transform.position += movement * speed * Time.deltaTime;
+
+        // Xoay nhân vật theo hướng di chuyển
+        if (moveHorizontal < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0); // Quay đầu sang trái
+        }
+        else if (moveHorizontal > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0); // Quay đầu sang phải
+        }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth); // Cập nhật thanh máu
+    }
+}
+
---
+++ Assets/Script/WeaponController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public string weaponName = "Default Weapon"; 
+    public int damage = 10; 
+    public float attackRange = 1.0f; 
+    public float attackSpeed = 1.0f; 
+    public AnimatorOverrideController weaponAnimator; 
+    public GameObject weaponModel; 
+    public int durability = 100; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
